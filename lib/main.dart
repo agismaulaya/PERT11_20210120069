813c5a1cfbdb88ea8ed4b5ba47e65d3f0a4cfa8f
Agis Maulaya <agisnimpp@gmail.com>
2023-07-08 12:42:52 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_form1/screens/register.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  Widget build(context) {
+    return MaterialApp(
+      title: 'Latihan Form, Navigasi Dan Validasi',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Silakan Input Form Registrasi'),
+        ),
+        body: RegisterScreen(),
+      ),
+    );
+  }
+}
